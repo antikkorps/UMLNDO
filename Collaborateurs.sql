9aadfc4ff20bc4cd9d9fe98f6072b12d59a90879
Franck Vienot <franckvienot7@gmail.com>
2022-09-08 16:42:15 +0200

    started building the sql file

---
+++ Collaborateurs.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS collaborateurs(
+  id_collaborateur int(11) NOT NULL,
+  actif boolean NOT NULL DEFAULT '1',
+  nom varchar(50) NOT NULL,
+  prenom varchar(50) NOT NULL, 
+);
+
+
+  INSERT INTO collaborateurs (id_collaborateur, nom, prenom, adresse, code_postal, ville) VALUES (52000, 'Muller', 'Karl', '28 place de l\'Eperon', '72000', 'Le Mans');
