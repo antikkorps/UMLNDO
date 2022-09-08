aa9bac0c71f1107439c603c3b2b42d668bf51a12
Franck Vienot <franckvienot7@gmail.com>
2022-09-08 17:34:14 +0200

    changes on sql

--- Collaborateurs.sql
+++ Collaborateurs.sql
@@ -5,10 +5,33 @@ CREATE TABLE IF NOT EXISTS collaborateurs(
   prenom varchar(50) NOT NULL, 
 );
 
+CREATE TABLE IF NOT EXISTS Directions_commerciales(
+    id_direction_co int(11) NOT NULL,
+    nom_direction_co varchar(100) NOT NULL,
+);
+
 CREATE TABLE IF NOT EXISTS secteurs(
     id_secteur int(11) NOT NULL,
-    nom_secteur varchar(50) NOT NULL
+    nom_secteur varchar(100) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS Groupement_pdv(
+    id_groupement_pdv int(11) NOT NULL,
+    nom_groupement_pdv varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pdv(
+    id_pdv int(11) NOT NULL,
+    nom_pdv varchar(100) NOT NULL,
+    /*
+    MATERIALISER LES ORGANISATIONS ACTUELLES, MODELISEES, TRANSITOIRES ET CIBLES
+    id_groupement_pdv int(11) NOT NULL,
+    id_secteur int(11) NOT NULL,
+    id_direction_co int(11) NOT NULL,
+    id_collaborateur int(11) NOT NULL
+    */
+);
+
 
 
 
