cb0ee93c9f0b9645ea2e47610a8ed76e641a6d15
antikkorps <franckvienot7@gmail.com>
2022-09-30 09:52:14 +0200

    start preparing insert datas

---
+++ data.sql
@@ -0,0 +1,17 @@
+INSERT INTO contrats (nom_contrat) 
+    VALUES ('CDI'),
+    VALUES ('CDD'),
+    VALUES ('Stage'),
+    VALUES ('Alternance'),
+    VALUES ('Intérim'),
+    VALUES ('Freelance'),
+    VALUES ('Apprentissage'),
+    VALUES ('Contrat de professionnalisation');
+
+INSERT INTO filieres (nom_filiere)
+    VALUES ('Particuliers'),
+    VALUES ('Pro/Agri'),
+    VALUES ('Entreprises'),
+    VALUES ('Patrimoine'),
+    VALUES ('Assurance');
+
