24d891120f9aa6e1e801c4cfeb363410e23ec539
antikkorps <franckvienot7@gmail.com>
2022-09-30 13:45:14 +0200

    start adding datas

--- data.sql
+++ data.sql
@@ -15,3 +15,67 @@ INSERT INTO filieres (nom_filiere)
     VALUES ('Patrimoine'),
     VALUES ('Assurance');
 
+INSERT INTO metiers (nom_metier)
+    VALUES ('Technico commercial particuliers'),
+    VALUES ('Technico commercial entreprises'),
+    VALUES ('Conseiller clientèle'),
+    VALUES ('Conseiller clientèle particuliers'),
+    VALUES ('Conseiller clientèle banque privée'),
+    VALUES ('Directeur de pôle délégué'),
+    VALUES ('Directeur de pôle'),
+    VALUES ('Chargé(e) accueil'),
+    VALUES ('Conseiller clientèle entreprises'),
+    VALUES ('Conseiller clientèle patrimoine'),
+    VALUES ('Conseiller clientèle assurance'),
+    VALUES ('Technico commercial pro'),
+    VALUES ('Technico commercial agri'),
+    VALUES ('Conseiller clientèle pro'),
+    VALUES ('Conseiller clientèle agri'),
+    VALUES ('Chargé de développement agricole'),
+    VALUES ('Chargé de développement pro'),
+    VALUES ('Directeur de pôle pro délégué'),
+    VALUES ('Directeur de pôle pro'),
+    VALUES ('Directeur de pôle agri délégué'),
+    VALUES ('Directeur de pôle agri'),
+    VALUES ('Technico commercial patrimoine'),
+    VALUES ('Conseiller clientèle patrimoine'),
+    VALUES ('Conseiller Gestion de patrimoine'),
+    VALUES ('Directeur de pôle patrimoine délégué'),
+    VALUES ('Directeur de pôle patrimoine');
+
+
+INSERT INTO activites (nom_activite)
+    VALUES ('Accueil'),
+    VALUES ('commerciale grand public'),
+    VALUES ('commerciale intermédiaires'),
+    VALUES ('Haut de gamme traditionnel'),
+    VALUES ('Associations'),
+    VALUES ('Pro grand public'),
+    VALUES ('Pro classique'),
+    VALUES ('Pro dynamique'),
+    VALUES ('Pro entreprenant'),
+    VALUES ('Agri grand public'),
+    VALUES ('Agri classique'),
+    VALUES ('Agri dynamique'),
+    VALUES ('Agri entreprenant'),
+    VALUES ('Agri CUMA'),
+    VALUES ('Agri SCAF')
+    VALUES ('Management');
+
+INSERT INTO etps (etp)
+    VALUES ('0.1'),
+    VALUES ('0.2'),
+    VALUES ('0.3'),
+    VALUES ('0.4'),
+    VALUES ('0.5'),
+    VALUES ('0.6'),
+    VALUES ('0.7'),
+    VALUES ('0.8'),
+    VALUES ('0.9'),
+    VALUES ('1');
+
+INSERT INTO pdv (nom_pdv)
+    VALUES (''),
+    VALUES (''),
+    VALUES (''),
+    VALUES ('');
