aefa43fecca13a9a72b98b54ad53eba36f6c0b04
Franck Vienot <franckvienot7@gmail.com>
2022-10-04 08:59:05 +0200

    change presentation of data

--- data.sql
+++ data.sql
@@ -1,327 +1,58 @@
 INSERT INTO contrats (nom_contrat) 
     VALUES 
-    ('A définir'),
-    ('CDI'),
-    ('CDD'),
-    ('Stage'),
-    ('Alternance'),
-    ('Intérim'),
-    ('Freelance'),
-    ('Apprentissage'),
-    ('Contrat de professionnalisation');
+    ('A définir'), ('CDI'), ('CDD'), ('Stage'), ('Alternance'), ('Intérim'), ('Freelance'), ('Apprentissage'), ('Contrat de professionnalisation');
 
 INSERT INTO collaborateur (matricule, nom, prenom)
     VALUES
- (69788,'NOM001','PRENOM001')
-,(63702,'NOM002','PRENOM002')
-,(69821,'NOM003','PRENOM003')
-,(62890,'NOM004','PRENOM004')
-,(69583,'NOM005','PRENOM005')
-,(62654,'NOM006','PRENOM006')
-,(69218,'NOM007','PRENOM007')
-,(99970,'NOM008','PRENOM008')
-,(68170,'NOM009','PRENOM009')
-,(68133,'NOM010','PRENOM010')
-,(68401,'NOM011','PRENOM011')
-,(64074,'NOM012','PRENOM012')
-,(69679,'NOM013','PRENOM013')
-,(69213,'NOM014','PRENOM014')
-,(68586,'NOM015','PRENOM015')
-,(90650,'NOM016','PRENOM016')
-,(63585,'NOM017','PRENOM017')
-,(69669,'NOM018','PRENOM018')
-,(70199,'NOM019','PRENOM019')
-,(28274,'NOM020','PRENOM020')
-,(63621,'NOM021','PRENOM021')
-,(69464,'NOM022','PRENOM022')
-,(62942,'NOM023','PRENOM023')
-,(64846,'NOM024','PRENOM024')
-,(69102,'NOM025','PRENOM025')
-,(69576,'NOM026','PRENOM026')
-,(64165,'NOM027','PRENOM027')
-,(99969,'NOM028','PRENOM028')
-,(86929,'NOM029','PRENOM029')
-,(69877,'NOM030','PRENOM030')
-,(69148,'NOM031','PRENOM031')
-,(69356,'NOM032','PRENOM032')
-,(63245,'NOM033','PRENOM033')
-,(69606,'NOM034','PRENOM034')
-,(69775,'NOM035','PRENOM035')
-,(69814,'NOM036','PRENOM036')
-,(68000,'NOM037','PRENOM037')
-,(68618,'NOM038','PRENOM038')
-,(68884,'NOM039','PRENOM039')
-,(68898,'NOM040','PRENOM040')
-,(68111,'NOM041','PRENOM041')
-,(90499,'NOM042','PRENOM042')
-,(70126,'NOM043','PRENOM043')
-,(87393,'NOM044','PRENOM044')
-,(64864,'NOM045','PRENOM045')
-,(63160,'NOM046','PRENOM046')
-,(69590,'NOM047','PRENOM047')
-,(91052,'NOM048','PRENOM048')
-,(23302,'NOM049','PRENOM049')
-,(69649,'NOM050','PRENOM050')
-,(69611,'NOM051','PRENOM051')
-,(68071,'NOM052','PRENOM052')
-,(69804,'NOM053','PRENOM053')
-,(99968,'NOM054','PRENOM054')
-,(91232,'NOM055','PRENOM055')
-,(69512,'NOM056','PRENOM056')
-,(99967,'NOM057','PRENOM057')
-,(63087,'NOM058','PRENOM058')
-,(69796,'NOM059','PRENOM059')
-,(62572,'NOM060','PRENOM060')
-,(64980,'NOM061','PRENOM061')
-,(91902,'NOM062','PRENOM062')
-,(70157,'NOM063','PRENOM063')
-,(69586,'NOM064','PRENOM064')
-,(64106,'NOM065','PRENOM065')
-,(62255,'NOM066','PRENOM066')
-,(90312,'NOM067','PRENOM067')
-,(69805,'NOM068','PRENOM068')
-,(68077,'NOM069','PRENOM069')
-,(63075,'NOM070','PRENOM070')
-,(64251,'NOM071','PRENOM071')
-,(68871,'NOM072','PRENOM072')
-,(64252,'NOM073','PRENOM073')
-,(64803,'NOM074','PRENOM074')
-,(62411,'NOM075','PRENOM075')
-,(69764,'NOM076','PRENOM076')
-,(69807,'NOM077','PRENOM077')
-,(70128,'NOM078','PRENOM078')
-,(63219,'NOM079','PRENOM079')
-,(64622,'NOM080','PRENOM080')
-,(69092,'NOM081','PRENOM081')
-,(69346,'NOM082','PRENOM082')
-,(64465,'NOM083','PRENOM083')
-,(69943,'NOM084','PRENOM084')
-,(70095,'NOM085','PRENOM085')
-,(99966,'NOM086','PRENOM086')
-,(69388,'NOM087','PRENOM087')
-,(69574,'NOM088','PRENOM088')
-,(69117,'NOM089','PRENOM089')
-,(29105,'NOM090','PRENOM090')
-,(68416,'NOM091','PRENOM091')
-,(69870,'NOM092','PRENOM092')
-,(64965,'NOM093','PRENOM093')
-,(64020,'NOM094','PRENOM094')
-,(21017,'NOM095','PRENOM095')
-,(63661,'NOM096','PRENOM096')
-,(64545,'NOM097','PRENOM097')
-,(64288,'NOM098','PRENOM098')
-,(21106,'NOM099','PRENOM099')
-,(68534,'NOM100','PRENOM100')
-,(69782,'NOM101','PRENOM101')
-,(99941,'NOM102','PRENOM102')
-,(68274,'NOM103','PRENOM103')
-,(63404,'NOM104','PRENOM104')
-,(63890,'NOM105','PRENOM105')
-,(69091,'NOM106','PRENOM106')
-,(24600,'NOM107','PRENOM107')
-,(63176,'NOM108','PRENOM108')
-,(63445,'NOM109','PRENOM109')
-,(63822,'NOM110','PRENOM110')
-,(62986,'NOM111','PRENOM111')
-,(63882,'NOM112','PRENOM112')
-,(64152,'NOM113','PRENOM113')
-,(62332,'NOM114','PRENOM114')
-,(63804,'NOM115','PRENOM115')
-,(68143,'NOM116','PRENOM116')
-,(90994,'NOM117','PRENOM117');
+ (69788,'NOM001','PRENOM001'),(63702,'NOM002','PRENOM002'),(69821,'NOM003','PRENOM003'),(62890,'NOM004','PRENOM004'),(69583,'NOM005','PRENOM005'),(62654,'NOM006','PRENOM006'),(69218,'NOM007','PRENOM007')
+,(99970,'NOM008','PRENOM008'),(68170,'NOM009','PRENOM009'),(68133,'NOM010','PRENOM010'),(68401,'NOM011','PRENOM011'),(64074,'NOM012','PRENOM012'),(69679,'NOM013','PRENOM013'),(69213,'NOM014','PRENOM014')
+,(68586,'NOM015','PRENOM015'),(90650,'NOM016','PRENOM016'),(63585,'NOM017','PRENOM017'),(69669,'NOM018','PRENOM018'),(70199,'NOM019','PRENOM019'),(28274,'NOM020','PRENOM020'),(63621,'NOM021','PRENOM021')
+,(69464,'NOM022','PRENOM022'),(62942,'NOM023','PRENOM023'),(64846,'NOM024','PRENOM024'),(69102,'NOM025','PRENOM025'),(69576,'NOM026','PRENOM026'),(64165,'NOM027','PRENOM027'),(99969,'NOM028','PRENOM028')
+,(86929,'NOM029','PRENOM029'),(69877,'NOM030','PRENOM030'),(69148,'NOM031','PRENOM031'),(69356,'NOM032','PRENOM032'),(63245,'NOM033','PRENOM033'),(69606,'NOM034','PRENOM034'),(69775,'NOM035','PRENOM035')
+,(69814,'NOM036','PRENOM036'),(68000,'NOM037','PRENOM037'),(68618,'NOM038','PRENOM038'),(68884,'NOM039','PRENOM039'),(68898,'NOM040','PRENOM040'),(68111,'NOM041','PRENOM041'),(90499,'NOM042','PRENOM042')
+,(70126,'NOM043','PRENOM043'),(87393,'NOM044','PRENOM044'),(64864,'NOM045','PRENOM045'),(63160,'NOM046','PRENOM046'),(69590,'NOM047','PRENOM047'),(91052,'NOM048','PRENOM048'),(23302,'NOM049','PRENOM049')
+,(69649,'NOM050','PRENOM050'),(69611,'NOM051','PRENOM051'),(68071,'NOM052','PRENOM052'),(69804,'NOM053','PRENOM053'),(99968,'NOM054','PRENOM054'),(91232,'NOM055','PRENOM055'),(69512,'NOM056','PRENOM056')
+,(99967,'NOM057','PRENOM057'),(63087,'NOM058','PRENOM058'),(69796,'NOM059','PRENOM059'),(62572,'NOM060','PRENOM060'),(64980,'NOM061','PRENOM061'),(91902,'NOM062','PRENOM062'),(70157,'NOM063','PRENOM063')
+,(69586,'NOM064','PRENOM064'),(64106,'NOM065','PRENOM065'),(62255,'NOM066','PRENOM066'),(90312,'NOM067','PRENOM067'),(69805,'NOM068','PRENOM068'),(68077,'NOM069','PRENOM069'),(63075,'NOM070','PRENOM070')
+,(64251,'NOM071','PRENOM071'),(68871,'NOM072','PRENOM072'),(64252,'NOM073','PRENOM073'),(64803,'NOM074','PRENOM074'),(62411,'NOM075','PRENOM075'),(69764,'NOM076','PRENOM076'),(69807,'NOM077','PRENOM077')
+,(70128,'NOM078','PRENOM078'),(63219,'NOM079','PRENOM079'),(64622,'NOM080','PRENOM080'),(69092,'NOM081','PRENOM081'),(69346,'NOM082','PRENOM082'),(64465,'NOM083','PRENOM083'),(69943,'NOM084','PRENOM084')
+,(70095,'NOM085','PRENOM085'),(99966,'NOM086','PRENOM086'),(69388,'NOM087','PRENOM087'),(69574,'NOM088','PRENOM088'),(69117,'NOM089','PRENOM089'),(29105,'NOM090','PRENOM090'),(68416,'NOM091','PRENOM091')
+,(69870,'NOM092','PRENOM092'),(64965,'NOM093','PRENOM093'),(64020,'NOM094','PRENOM094'),(21017,'NOM095','PRENOM095'),(63661,'NOM096','PRENOM096'),(64545,'NOM097','PRENOM097'),(64288,'NOM098','PRENOM098')
+,(21106,'NOM099','PRENOM099'),(68534,'NOM100','PRENOM100'),(69782,'NOM101','PRENOM101'),(99941,'NOM102','PRENOM102'),(68274,'NOM103','PRENOM103'),(63404,'NOM104','PRENOM104'),(63890,'NOM105','PRENOM105')
+,(69091,'NOM106','PRENOM106'),(24600,'NOM107','PRENOM107'),(63176,'NOM108','PRENOM108'),(63445,'NOM109','PRENOM109'),(63822,'NOM110','PRENOM110'),(62986,'NOM111','PRENOM111'),(63882,'NOM112','PRENOM112')
+,(64152,'NOM113','PRENOM113'),(62332,'NOM114','PRENOM114'),(63804,'NOM115','PRENOM115'),(68143,'NOM116','PRENOM116'),(90994,'NOM117','PRENOM117');
 
 INSERT INTO filieres (nom_filiere)
     VALUES
-    ('A définir'),
-    ('Particuliers'),
-    ('Pro/Agri'),
-    ('Entreprises'),
-    ('Patrimoine'),
-    ('Assurance');
+    ('A définir'), ('Particuliers'), ('Pro/Agri'), ('Entreprises'), ('Patrimoine'), ('Assurance');
 
 INSERT INTO metiers (nom_metier)
     VALUES 
-    ('A définir'),
-    ('Technico commercial particuliers'),
-    ('Conseiller clientèle'),
-    ('Technico commercial entreprises'),
-    ('Conseiller clientèle particuliers'),
-    ('Conseiller clientèle banque privée'),
-    ('Directeur de pôle délégué'),
-    ('Directeur de pôle'),
-    ('Chargé(e) accueil'),
-    ('Conseiller clientèle entreprises'),
-    ('Conseiller clientèle patrimoine'),
-    ('Conseiller clientèle assurance'),
-    ('Technico commercial pro'),
-    ('Technico commercial agri'),
-    ('Conseiller clientèle pro'),
-    ('Conseiller clientèle agri'),
-    ('Chargé de développement agricole'),
-    ('Chargé de développement pro'),
-    ('Directeur de pôle pro délégué'),
-    ('Directeur de pôle pro'),
-    ('Directeur de pôle agri délégué'),
-    ('Directeur de pôle agri'),
-    ('Technico commercial patrimoine'),
-    ('Conseiller clientèle patrimoine'),
-    ('Conseiller Gestion de patrimoine'),
-    ('Directeur de pôle patrimoine délégué'),
-    ('Directeur de pôle patrimoine');
+    ('A définir'), ('Technico commercial particuliers'), ('Conseiller clientèle'), ('Technico commercial entreprises'), ('Conseiller clientèle particuliers'), ('Conseiller clientèle banque privée'), 
+    ('Directeur de pôle délégué'), ('Directeur de pôle'), ('Chargé(e) accueil'), ('Conseiller clientèle entreprises'), ('Conseiller clientèle patrimoine'), ('Conseiller clientèle assurance'), 
+    ('Technico commercial pro'), ('Technico commercial agri'), ('Conseiller clientèle pro'), ('Conseiller clientèle agri'), ('Chargé de développement agricole'), ('Chargé de développement pro'), 
+    ('Directeur de pôle pro délégué'), ('Directeur de pôle pro'), ('Directeur de pôle agri délégué'), ('Directeur de pôle agri'), ('Technico commercial patrimoine'), ('Conseiller clientèle patrimoine'), 
+    ('Conseiller Gestion de patrimoine'), ('Directeur de pôle patrimoine délégué'), ('Directeur de pôle patrimoine');
 
 
 INSERT INTO activites (nom_activite)
     VALUES
-    ('A définir'),
-    ('Accueil'),
-    ('commerciale grand public'),
-    ('commerciale intermédiaires'),
-    ('Haut de gamme traditionnel'),
-    ('Associations'),
-    ('Pro grand public'),
-    ('Pro classique'),
-    ('Pro dynamique'),
-    ('Pro entreprenant'),
-    ('Agri grand public'),
-    ('Agri classique'),
-    ('Agri dynamique'),
-    ('Agri entreprenant'),
-    ('Agri CUMA'),
-    ('Agri SCAF'),
-    ('Management');
+    ('A définir'), ('Accueil'), ('commerciale grand public'), ('commerciale intermédiaires'), ('Haut de gamme traditionnel'), ('Associations'), ('Pro grand public'), ('Pro classique'), ('Pro dynamique'), 
+    ('Pro entreprenant'), ('Agri grand public'), ('Agri classique'), ('Agri dynamique'), ('Agri entreprenant'), ('Agri CUMA'), ('Agri SCAF'), ('Management');
 
 INSERT INTO etps (etp)
-    VALUES
-    ('A définir'),
-    (0.01),
-    (0.02),
-    (0.03),
-    (0.04),
-    (0.05),
-    (0.06),
-    (0.07),
-    (0.08),
-    (0.09),
-    (0.10),
-    (0.11),
-    (0.12),
-    (0.13),
-    (0.14),
-    (0.15),
-    (0.16),
-    (0.17),
-    (0.18),
-    (0.19),
-    (0.20),
-    (0.21),
-    (0.22),
-    (0.23),
-    (0.24),
-    (0.25),
-    (0.26),
-    (0.27),
-    (0.28),
-    (0.29),
-    (0.30),
-    (0.31),
-    (0.32),
-    (0.33),
-    (0.34),
-    (0.35),
-    (0.36),
-    (0.37),
-    (0.38),
-    (0.39),
-    (0.40),
-    (0.41),
-    (0.42),
-    (0.43),
-    (0.44),
-    (0.45),
-    (0.46),
-    (0.47),
-    (0.48),
-    (0.49),
-    (0.50),
-    (0.51),
-    (0.52),
-    (0.53),
-    (0.54),
-    (0.55),
-    (0.56),
-    (0.57),
-    (0.58),
-    (0.59),
-    (0.60),
-    (0.61),
-    (0.62),
-    (0.63),
-    (0.64),
-    (0.65),
-    (0.66),
-    (0.67),
-    (0.68),
-    (0.69),
-    (0.70),
-    (0.71),
-    (0.72),
-    (0.73),
-    (0.74),
-    (0.75),
-    (0.76),
-    (0.77),
-    (0.78),
-    (0.79),
-    (0.80),
-    (0.81),
-    (0.82),
-    (0.83),
-    (0.84),
-    (0.85),
-    (0.86),
-    (0.87),
-    (0.88),
-    (0.89),
-    (0.90),
-    (0.91),
-    (0.92),
-    (0.93),
-    (0.94),
-    (0.95),
-    (0.96),
-    (0.97),
-    (0.98),
-    (0.99);
+    VALUES 
+    ('A définir'), (0.01), (0.02), (0.03), (0.04), (0.05), (0.06), (0.07), (0.08), (0.09), (0.10), (0.11), (0.12), (0.13), (0.14), (0.15), (0.16), (0.17), (0.18), (0.19), (0.20), (0.21), (0.22), 
+    (0.23), (0.24), (0.25), (0.26), (0.27), (0.28), (0.29), (0.30), (0.31), (0.32), (0.33), (0.34), (0.35), (0.36), (0.37), (0.38), (0.39), (0.40), (0.41), (0.42), (0.43), (0.44), (0.45), (0.46), 
+    (0.47), (0.48), (0.49), (0.50), (0.51), (0.52), (0.53), (0.54), (0.55), (0.56), (0.57), (0.58), (0.59), (0.60), (0.61), (0.62), (0.63), (0.64), (0.65), (0.66), (0.67), (0.68), (0.69), (0.70), 
+    (0.71), (0.72), (0.73), (0.74), (0.75), (0.76), (0.77), (0.78), (0.79), (0.80), (0.81), (0.82), (0.83), (0.84), (0.85), (0.86), (0.87), (0.88), (0.89), (0.90), (0.91), (0.92), (0.93), (0.94), 
+    (0.95), (0.96), (0.97), (0.98), (0.99);
 
 INSERT INTO pdv (id_pdv, nom_pdv)
     VALUES 
-    (172,'BELF_VIEILLE_VILLE'),
-    (175,'BELFORT JEAN-JAURES'),
-    (176,'BESSONCOURT'),
-    (177,'#N/A'),
-    (178,'GIROMAGNY'),
-    (324,'VALDOIE'),
-    (402,'GRANDVILLARS'),
-    (403,'AUDINCOURT'),
-    (404,'HERICOURT'),
-    (405,'HERIMONCOURT'),
-    (416,'SELONCOURT'),
-    (422,'COLOMBIER-FONTAINE'),
-    (424,'VALENTIGNEY'),
-    (426,'BAVANS'),
-    (428,'BELFORT CENTRE'),
-    (432,'PONT DE ROIDE'),
-    (436,'DANJOUTIN'),
-    (440,'ESSERT'),
-    (522,'MONTBELIARD CENTRE'),
-    (523,'BEAUCOURT'),
-    (541,'DELLE'),
-    (542,'FESCHES LE CHATEL'),
-    (543,'ISLE/DOUBS'),
-    (545,'Siège'),
-    (547,'GRAND-CHARMONT'),
-    (548,'MONTBE. PIED DES GOUTTES'),
-    (549,'SOCHAUX'),
-    (605,'POLE AGRI PRO BELFORT');
+    (172,'BELF_VIEILLE_VILLE'), (175,'BELFORT JEAN-JAURES'), (176,'BESSONCOURT'), (177,'#N/A'), (178,'GIROMAGNY'), (324,'VALDOIE'), (402,'GRANDVILLARS'), (403,'AUDINCOURT'), (404,'HERICOURT'), 
+    (405,'HERIMONCOURT'), (416,'SELONCOURT'), (422,'COLOMBIER-FONTAINE'), (424,'VALENTIGNEY'), (426,'BAVANS'), (428,'BELFORT CENTRE'), (432,'PONT DE ROIDE'), (436,'DANJOUTIN'), (440,'ESSERT'), 
+    (522,'MONTBELIARD CENTRE'), (523,'BEAUCOURT'), (541,'DELLE'), (542,'FESCHES LE CHATEL'), (543,'ISLE/DOUBS'), (545,'Siège'), (547,'GRAND-CHARMONT'), (548,'MONTBE. PIED DES GOUTTES'), 
+    (549,'SOCHAUX'), (605,'POLE AGRI PRO BELFORT');
 
 
